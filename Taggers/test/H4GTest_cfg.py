882eafd60c65b1d558fad06c17cb1dcb6e6a42b1
wamorkart <wamorkar.t@husky.neu.edu>
2019-07-03 19:38:30 +0200

    access parent dataset using fggrunjobs

--- Taggers/test/H4GTest_cfg.py
+++ Taggers/test/H4GTest_cfg.py
@@ -1,50 +1,83 @@
-
 import subprocess
 import FWCore.ParameterSet.Config as cms
 import os
 
-from FWCore.ParameterSet.VarParsing import VarParsing
+# from FWCore.ParameterSet.VarParsing import VarParsing
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 from flashgg.Taggers.flashggH4GCandidate_cfi import FlashggH4GCandidate
 from flashgg.Taggers.flashggPreselectedDiPhotons_cfi import flashggPreselectedDiPhotons
 from flashgg.Taggers.flashggPreselectedDiPhotons_LowMass_cfi import flashggPreselectedDiPhotonsLowMass
 import flashgg.Taggers.dumperConfigTools as cfgTools
 
-options = VarParsing('analysis')
-options.register('dataset',
+process = cms.Process("FLASHggH4GTest")
+
+
+from flashgg.MetaData.JobConfig import customize
+customize.options.register('inputdataset',
                  '',
-                 VarParsing.multiplicity.list,
-                 VarParsing.varType.string,
+                 VarParsing.VarParsing.multiplicity.list,
+                 VarParsing.VarParsing.varType.string,
                  "Input dataset(s)")
 
-options.register('campaign',
+customize.options.register('stdDumper',
                  '',
-                 VarParsing.multiplicity.singleton,
-                 VarParsing.varType.string,
-                 "campaign")
-
-options.register('stdDumper',
-                 '',
-                 VarParsing.multiplicity.singleton,
-                 VarParsing.varType.bool,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
                  "stdDumper")
 
-options.register('vtxBDTDumper',
+customize.options.register('vtxBDTDumper',
                  '',
-                 VarParsing.multiplicity.singleton,
-                 VarParsing.varType.bool,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
                  "vtxBDTDumper")
 
-options.register('vtxProbDumper',
+customize.options.register('vtxProbDumper',
                  '',
-                 VarParsing.multiplicity.singleton,
-                 VarParsing.varType.bool,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
                  "vtxProbDumper")
 
-options.parseArguments()
+from flashgg.MetaData.JobConfig import customize
+
+customize.setDefault("maxEvents",-1)
+customize.setDefault("targetLumi",1e+3)
+customize.parse()
+# options = VarParsing('analysis')
+# options.register('dataset',
+#                  '',
+#                  VarParsing.multiplicity.list,
+#                  VarParsing.varType.string,
+#                  "Input dataset(s)")
+#
+# options.register('campaign',
+#                  '',
+#                  VarParsing.multiplicity.singleton,
+#                  VarParsing.varType.string,
+#                  "campaign")
+#
+# options.register('stdDumper',
+#                  '',
+#                  VarParsing.multiplicity.singleton,
+#                  VarParsing.varType.bool,
+#                  "stdDumper")
+#
+# options.register('vtxBDTDumper',
+#                  '',
+#                  VarParsing.multiplicity.singleton,
+#                  VarParsing.varType.bool,
+#                  "vtxBDTDumper")
+#
+# options.register('vtxProbDumper',
+#                  '',
+#                  VarParsing.multiplicity.singleton,
+#                  VarParsing.varType.bool,
+#                  "vtxProbDumper")
+#
+# options.parseArguments()
 
 
-process = cms.Process("FLASHggH4GTest")
+# process = cms.Process("FLASHggH4GTest")
 
 # process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
@@ -128,10 +161,12 @@ cfgTools.addCategories(process.h4gCandidateDumper,
                         variables = all_variables,
                         histograms=[]
                         )
+from flashgg.MetaData.JobConfig import customize
 
 files = []
 secondary_files = []
-for d in options.dataset:
+
+for d in customize.inputdataset:
     print('>> Creating list of files from: \n'+d)
     for instance in ['global', 'phys03']:
         query = "-query='file dataset="+d+" instance=prod/"+instance+"'"
@@ -166,12 +201,12 @@ process.TFileService = cms.Service("TFileService",
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 ##--import flashgg customization
-from flashgg.MetaData.JobConfig import customize
-import FWCore.ParameterSet.VarParsing as VarParsing
+# from flashgg.MetaData.JobConfig import customize
+# import FWCore.ParameterSet.VarParsing as VarParsing
 
 ##--set default options
-customize.setDefault("maxEvents",-1)
-customize.setDefault("targetLumi",1e+3)
+# customize.setDefault("maxEvents",-1)
+# customize.setDefault("targetLumi",1e+3)
 # customize.register('PURW',
 # 				1,
 # 				VarParsing.VarParsing.multiplicity.singleton,
@@ -185,7 +220,7 @@ customize.setDefault("targetLumi",1e+3)
 	# process.h4gCandidateDumper.puTarget = cms.vdouble()
 #customize.setDefault("puTarget",'2.39e+05,8.38e+05,2.31e+06,3.12e+06,4.48e+06,6e+06,7e+06,1.29e+07,3.53e+07,7.87e+07,1.77e+08,3.6e+08,6.03e+08,8.77e+08,1.17e+09,1.49e+09,1.76e+09,1.94e+09,2.05e+09,2.1e+09,2.13e+09,2.15e+09,2.13e+09,2.06e+09,1.96e+09,1.84e+09,1.7e+09,1.55e+09,1.4e+09,1.24e+09,1.09e+09,9.37e+08,7.92e+08,6.57e+08,5.34e+08,4.27e+08,3.35e+08,2.58e+08,1.94e+08,1.42e+08,1.01e+08,6.9e+07,4.55e+07,2.88e+07,1.75e+07,1.02e+07,5.64e+06,2.99e+06,1.51e+06,7.32e+05,3.4e+05,1.53e+05,6.74e+04,3.05e+04,1.52e+04,8.98e+03,6.5e+03,5.43e+03,4.89e+03,4.52e+03,4.21e+03,3.91e+03,3.61e+03,3.32e+03,3.03e+03,2.75e+03,2.47e+03,2.21e+03,1.97e+03,1.74e+03,1.52e+03,1.32e+03,1.14e+03,983,839')
 
-customize(process)
+# customize(process)
 if customize.inputFiles:
     inputFile = customize.inputFiles
 
@@ -217,14 +252,16 @@ if customize.processId == "Data":
 
 process.load("flashgg/Taggers/vtxH4GSequence")
 
-if options.stdDumper:
+if customize.stdDumper:
    #standard dumper sequence
    process.path = cms.Path(process.vtxH4GSequence*process.dataRequirements*process.FlashggH4GCandidate*process.h4gCandidateDumper)
 
-if options.vtxBDTDumper:
+if customize.vtxBDTDumper:
    #vtxBDT dumper sequence
    process.path = cms.Path(process.vtxH4GSequence*process.dataRequirements*process.FlashggH4GCandidate*process.h4gCandidateDumper_vtxBDT_sig*process.h4gCandidateDumper_vtxBDT_bkg)
 
-if options.vtxProbDumper:
+if customize.vtxProbDumper:
    #vtxProb dumper sequence
    process.path = cms.Path(process.vtxH4GSequence*process.dataRequirements*process.FlashggH4GCandidate*process.h4gCandidateDumper_vtxProb)
+
+customize(process)
