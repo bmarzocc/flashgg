41bda6b57eccb9369742aaaefedea8a552ca4cc7
wamorkart <wamorkar.t@husky.neu.edu>
2019-07-03 16:30:47 +0200

    job submission not working with condor

--- Taggers/test/H4GTest_cfg.py
+++ Taggers/test/H4GTest_cfg.py
@@ -11,7 +11,7 @@ from flashgg.Taggers.flashggPreselectedDiPhotons_LowMass_cfi import flashggPrese
 import flashgg.Taggers.dumperConfigTools as cfgTools
 
 options = VarParsing('analysis')
-options.register('processes',
+options.register('dataset',
                  '',
                  VarParsing.multiplicity.list,
                  VarParsing.varType.string,
@@ -131,10 +131,10 @@ cfgTools.addCategories(process.h4gCandidateDumper,
 
 files = []
 secondary_files = []
-for p in options.processes:
-    print('>> Creating list of files from: \n'+p)
+for d in options.dataset:
+    print('>> Creating list of files from: \n'+d)
     for instance in ['global', 'phys03']:
-        query = "-query='file dataset="+p+" instance=prod/"+instance+"'"
+        query = "-query='file dataset="+d+" instance=prod/"+instance+"'"
         lsCmd = subprocess.Popen(['dasgoclient '+query+' -limit=0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         str_files, err = lsCmd.communicate()
         files.extend(['root://cms-xrd-global.cern.ch/'+ifile for ifile in str_files.split("\n")])
@@ -149,19 +149,16 @@ for p in options.processes:
 for f in files:
     print f
     print " "
-print "HERE 1"
 
 for s in secondary_files:
     print s
     print " "
 
-print "HERE 2"
 process.source = cms.Source ("PoolSource",
                              fileNames = cms.untracked.vstring(files),
                              secondaryFileNames = cms.untracked.vstring(secondary_files)
 )
 
-print "HERE 3"
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("test.root"),
                                    closeFileFast = cms.untracked.bool(True)
@@ -188,13 +185,10 @@ customize.setDefault("targetLumi",1e+3)
 	# process.h4gCandidateDumper.puTarget = cms.vdouble()
 #customize.setDefault("puTarget",'2.39e+05,8.38e+05,2.31e+06,3.12e+06,4.48e+06,6e+06,7e+06,1.29e+07,3.53e+07,7.87e+07,1.77e+08,3.6e+08,6.03e+08,8.77e+08,1.17e+09,1.49e+09,1.76e+09,1.94e+09,2.05e+09,2.1e+09,2.13e+09,2.15e+09,2.13e+09,2.06e+09,1.96e+09,1.84e+09,1.7e+09,1.55e+09,1.4e+09,1.24e+09,1.09e+09,9.37e+08,7.92e+08,6.57e+08,5.34e+08,4.27e+08,3.35e+08,2.58e+08,1.94e+08,1.42e+08,1.01e+08,6.9e+07,4.55e+07,2.88e+07,1.75e+07,1.02e+07,5.64e+06,2.99e+06,1.51e+06,7.32e+05,3.4e+05,1.53e+05,6.74e+04,3.05e+04,1.52e+04,8.98e+03,6.5e+03,5.43e+03,4.89e+03,4.52e+03,4.21e+03,3.91e+03,3.61e+03,3.32e+03,3.03e+03,2.75e+03,2.47e+03,2.21e+03,1.97e+03,1.74e+03,1.52e+03,1.32e+03,1.14e+03,983,839')
 
-print "HERE 4"
 customize(process)
-print "HERE 5"
 if customize.inputFiles:
     inputFile = customize.inputFiles
 
-print "HERE 5"
 # Require low mass diphoton triggers
 from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
 process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring(
@@ -223,7 +217,6 @@ if customize.processId == "Data":
 
 process.load("flashgg/Taggers/vtxH4GSequence")
 
-print "HERE 5"
 if options.stdDumper:
    #standard dumper sequence
    process.path = cms.Path(process.vtxH4GSequence*process.dataRequirements*process.FlashggH4GCandidate*process.h4gCandidateDumper)
